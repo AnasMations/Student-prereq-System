b52a5f68b25b86cf42cac41b25c3bb398c6fe42f
AnasMations <anas.ahassan@yahoo.com>
2022-01-26 16:34:29 +0200

    Added function change_to_int

--- main.cpp
+++ main.cpp
@@ -35,12 +35,45 @@ void extract_columns(string fileName, vector<string> &column1, vector<string> &c
     }
 }
 
+//iterate over every char of the string (better than isdigit())
+bool is_number(string str)
+{
+    for (char const &c : str)
+    {
+        if (std::isdigit(c) == 0) return false;
+    }
+    return true;
+}
+
+//get first 3 digits in the string
+vector<int> change_to_int(vector<string> column_str)
+{
+    vector<int> column_int;
+
+    for(int i=0; i<column_str.size(); i++)
+    {
+        if( is_number( column_str[i].substr(0, 3) ) )
+        {
+            column_int.push_back( stoi(column_str[i].substr(0, 3)) );
+        }else
+        {
+            column_int.push_back(0);
+        }
+    }
+
+    return column_int;
+}
+
 int main()
 {
-    vector<string> column1, column2;
+    vector<string> column1_str, column2_str;
+    vector<int> column1, column2;
 
-    extract_columns("Courses.csv", column1, column2);
+    extract_columns("Courses.csv", column1_str, column2_str);
 
-    cout<<column1[9]<<endl<<column2[9];
+    column1 = change_to_int(column1_str);
+    column2 = change_to_int(column2_str);
+
+    cout<<column1[9];
 
 }
